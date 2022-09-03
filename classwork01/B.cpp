98508443e9a43ff90d087e6401d57e437923be36
Elina <elinamuhamedova13@gmail.com>
2022-09-03 16:58:05 +0300

    error: array has incomplete element type 'void' error: arithmetic on a pointer to void

--- classwork01/B.cpp
+++ classwork01/B.cpp
@@ -8,9 +8,28 @@
 #include <iostream>
 
 
-int compare_
+int compare_int(void* first, void* second) {
+    return (reinterpret_cast<int*>(first)) - (reinterpret_cast<int*>(second));
+}
 
 
 int min(void arr[], int arr_size, int type_size, int (*compare)(void *, void *)) {
-    
+    void* ans = arr;
+    void* ptr = arr;
+    int k = 0;
+    for (int i = 0; i < arr_size; i++) {
+        if (compare(ptr, ans) < 0) {
+            ans = ptr;
+            k = i;
+        }
+        ptr += type_size;
+    }
+    return k;
+}
+
+
+int main() {
+    int arr[5] = {5, 7, 4, 8, -1};
+    std::cout << min(arr, 5, sizeof(arr[0]), compare_int);
+    return 0;
 }
