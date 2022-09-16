d48c555214a150b7ff421b8406669a6f4c83f8c6
Elina <elinamuhamedova13@gmail.com>
2022-09-16 21:08:37 +0300

    Fraction (homework)

--- classwork02/fraction.cpp
+++ classwork02/fraction.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include <cstdlib>
 
 class Fraction {
 public:
@@ -15,7 +16,16 @@ public:
     Fraction(const Fraction& other) = default;  // : Fraction(other.numerator, other.denominator) { }
     
     void printFraction() {
-        std::cout << numerator << " / " << denominator << std::endl;
+        if ((numerator > 0 and denominator > 0) or (numerator < 0 and denominator < 0))
+            std::cout << abs(numerator) << " / " << abs(denominator) << std::endl;
+        else
+            if (numerator != 0 and denominator != 0)
+                std::cout << -abs(numerator) << " / " << abs(denominator) << std::endl;
+            else
+                if (denominator == 0)
+                    std::cout << "division by 0" << std::endl;
+                else
+                    std::cout << 0 << std::endl;
     }
     void fracReverse() {
         int x = numerator;
@@ -23,9 +33,9 @@ public:
         denominator = x;
     }
     void fracSimplify() {
-        int a = denominator;
-        int b = numerator;
-        if (numerator < denominator) {
+        int a = abs(denominator);
+        int b = abs(numerator);
+        if (b < a) {
             a = numerator;
             b = denominator;
         }
@@ -53,9 +63,52 @@ public:
         numerator *= value;
     }
     void multByFrac(const Fraction& other) {
-        
+        this->numerator *= other.numerator;
+        this->denominator *= other.denominator;
     }
-    
+    Fraction multByFracReturn(const Fraction& other) const {
+        int new_numerator = this->numerator * other.numerator;
+        int new_denominator = this->denominator * other.denominator;
+        return Fraction(new_numerator, new_denominator);
+            }
+    void sumFrac(const Fraction& other) {
+        int a = this->numerator;
+        int b = this->denominator;
+        this->numerator = a * other.denominator + b * other.numerator;
+        this->denominator = b * other.denominator;
+            }
+    Fraction sumFracReturn(const Fraction& other) const {
+        int a = this->numerator;
+        int b = this->denominator;
+        int new_numerator = a * other.denominator + b * other.numerator;
+        int new_denominator = b * other.denominator;
+        return Fraction(new_numerator, new_denominator);
+            }
+    void diffFrac(const Fraction& other) {
+        int a = this->numerator;
+        int b = this->denominator;
+        this->numerator = a * other.denominator - b * other.numerator;
+        this->denominator = b * other.denominator;
+            }
+    Fraction diffFracReturn(const Fraction& other) const {
+        int a = this->numerator;
+        int b = this->denominator;
+        int new_numerator = a * other.denominator - b * other.numerator;
+        int new_denominator = b * other.denominator;
+        return Fraction(new_numerator, new_denominator);
+            }
+    double calc() {
+        if (denominator != 0)
+            return double(numerator) / double(denominator);
+        else
+            return 0;
+    }
+    int getNumerator() {
+        return numerator;
+            }
+    int getDenominator() {
+        return denominator;
+            }
 private:
     int numerator;
     int denominator;
@@ -90,4 +143,46 @@ int main() {
     first.printFraction();
     second.printFraction();
     std::cout << std::endl;
+    
+    first.multByFrac(Fraction(3, 7));
+    second.multByFrac(Fraction(5, 9));
+    
+    first.printFraction();
+    second.printFraction();
+    std::cout << std::endl;
+    
+    Fraction third = first.multByFracReturn(second);
+    third.printFraction();
+    std::cout << std::endl;
+    
+    first.fracSimplify();
+    second.fracSimplify();
+    third.fracSimplify();
+    first.printFraction();
+    second.printFraction();
+    third.printFraction();
+    std::cout << std::endl;
+    
+    Fraction fourth = Fraction(3, 7);
+    fourth.diffFrac(Fraction(4, 2));
+    fourth.printFraction();
+    std::cout << std::endl;
+    
+    fourth.fracSimplify();
+    fourth.printFraction();
+    std::cout << fourth.calc() << std::endl;
+    std::cout << std::endl;
+    
+    Fraction fifth = Fraction(1, 5);
+    Fraction sixth = Fraction(2, 5);
+    Fraction seventh = fifth.diffFracReturn(sixth);
+    
+    seventh.fracSimplify();
+    seventh.printFraction();
+    std::cout << seventh.calc() << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << seventh.getNumerator() << std::endl;
+    std::cout << seventh.getDenominator() << std::endl;
+    
 }
