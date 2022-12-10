0d5dcad3aff77456502204f8a5c5cba25bdd40fb
Elina <elinamuhamedova13@gmail.com>
2022-12-10 12:31:47 +0300

    December, 10th

--- labs05/grid.cpp
+++ labs05/grid.cpp
@@ -7,30 +7,184 @@
 
 #include <iostream>
 
-template <typename T>
-class Grid final {
-    public :
-        using value_type = T;
-        using size_type = unsigned;
-    private :
-        T * const data;
-        size_type const y_size , x_size ;
-        Grid(T *data , size_type y_size , size_type x_size ) : data(data), y_size(y_size), x_size(x_size) { }
-    Grid(Grid<T> const &) = delete;
-    Grid(Grid<T>&&)= delete;
-    Grid<T>& operator=(Grid<T>&) = delete;
-    Grid<T>& operator=(Grid<T>&&) = delete;
-    T operator ()(size_type y_idx, size_type x_idx) const {
-        return data [ y_idx * x_size + x_idx ];
+template<class T>
+static void destructArray(T* arr, size_t len) noexcept {
+    for (size_t i = len; i > 0; i--) {
+        arr[i - 1].~T();
     }
-    T& operator ()(size_type y_idx , size_type x_idx) {
-        return data [ y_idx * x_size + x_idx ];
+}
+
+template <typename T, unsigned dim>
+struct Grid final {
+    using value_type = T;
+    using size_type = unsigned;
+
+    template<class U = T>
+    Grid(const T& val, typename std::enable_if<dim == 2, U>::type* = 0): Grid(1, 1, val) {}
+
+    template<class... Args>
+    Grid(size_type x, Args... args): data(static_cast<Grid<T, dim - 1>*>(operator new(sizeof(Grid<T, dim - 1>) * x))),
+                                    sz(x) {
+        size_type done = 0;
+        try {
+            for (; done < sz; done++) {
+                new(data + done) Grid<T, dim - 1>(args...);
+            }
+        } catch (...) {
+            destructArray(data, done);
+            throw;
+        }
     }
-    Grid<T>& operator=(T const &t) {
-        for (auto it = data, end = data + x_size * y_size; it != end; ++it)
-            *it=t;
+
+    Grid(const Grid<T, dim> &other):
+            data(static_cast<Grid<T, dim - 1>*>(operator new(sizeof(Grid<T, dim - 1>) * other.sz))),
+            sz(other.sz) {
+        size_type done = 0;
+        try {
+            for (; done < sz; done++) {
+                new(data + done) Grid<T, dim - 1>(other[done]);
+            }
+        } catch (...) {
+            destructArray(data, done);
+            throw;
+        }
+    }
+
+    Grid<T, dim>& operator=(const Grid<T, dim>& other) {
+        if (this == &other) {
+            return *this;
+        }
+        auto gridCopy = other;
+        this->swap(gridCopy);
         return *this;
     }
-    size_type get_y_size () const { return y_size ; }
-    size_type get_x_size () const { return x_size ; }
+
+    ~Grid() {
+        destructArray(data, sz);
+        operator delete(data);
+    }
+
+    void swap(Grid<T, dim> &other) {
+        std::swap(data, other.data);
+        std::swap(sz, other.sz);
+    }
+
+    template<class... Args>
+    T operator ()(size_type x, Args... rst) const {
+    }
+
+    template<class... Args>
+    T& operator ()(size_type x, Args... rst) {
+        return data[x](rst...);
+    }
+
+    Grid<T, dim - 1> operator[](size_type x) const {
+        return data[x];
+    }
+
+    Grid<T, dim - 1>& operator[](size_type x) {
+        return data[x];
+    }
+
+    template<size_type d>
+    size_type get_size() const {
+        if constexpr(d == 1) {
+            return sz;
+        } else {
+            return data[0].template get_size<d - 1>();
+        }
+    }
+
+private :
+    Grid<T, dim - 1>* data;
+    size_type sz;
+
 };
+
+template <typename T>
+struct Grid<T, 1> final {
+    using value_type = T;
+    using size_type = unsigned;
+
+    Grid(size_type x): data(static_cast<T*>(operator new(sizeof(T) * x))), sz(x) {
+        size_type done = 0;
+        try {
+            for (; done < sz; done++) {
+                new(data + done) T();
+            }
+        } catch (...) {
+            destructArray(data, done);
+            throw;
+        }
+    }
+
+    Grid(size_type x, const T& val): data(static_cast<T*>(operator new(sizeof(T) * x))), sz(x) {
+        size_type done = 0;
+        try {
+            for (; done < sz; done++) {
+                new(data + done) T(val);
+            }
+        } catch (...) {
+            destructArray(data, done);
+            throw;
+        }
+    }
+
+    Grid(const Grid<T, 1> &other): data(static_cast<T*>(operator new(sizeof(T) * other.sz))), sz(other.sz) {
+        size_type done = 0;
+        try {
+            for (; done < sz; done++) {
+                new(data + done) T(other[done]);
+            }
+        } catch (...) {
+            destructArray(data, done);
+            throw;
+        }
+    }
+
+    Grid<T, 1>& operator=(const Grid<T, 1>& other) {
+        if (this == &other) {
+            return *this;
+        }
+        auto gridCopy = other;
+        this->swap(gridCopy);
+        return *this;
+    }
+
+    ~Grid() {
+        destructArray(data, sz);
+        operator delete(data);
+    }
+
+    void swap(Grid<T, 1> &other) {
+        std::swap(data, other.data);
+        std::swap(sz, other.sz);
+    }
+
+    T operator ()(size_type x) const {
+        return data[x];
+    }
+
+    T& operator ()(size_type x) {
+        return data[x];
+    }
+
+    T operator[](size_type x) const {
+        return data[x];
+    }
+
+    T& operator[](size_type x) {
+        return data[x];
+    }
+
+    template<size_type d>
+    size_type get_size() const {
+        static_assert(d == 1, "wrong dimension");
+        return sz;
+    }
+
+private :
+    T* data;
+    size_type sz;
+};
+
